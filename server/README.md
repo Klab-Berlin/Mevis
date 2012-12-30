444fd208068f2fdfb0faeb42b6ef41260b10bf42
Autarc <Autarc@gmail.com>
2012-12-30 02:36:50 +0100

    update server config

---
+++ server/README.md
@@ -0,0 +1,5 @@
+mevis-server
+============
+
+The Mevis server, which provides the backend for the mesh visualizer and is a spot
+by itself.
--- server/config.js
+++ server/config.js
@@ -4,7 +4,10 @@
 var config = {
 
 	host: 'mevis.dev',
-	port: 2020
+	port: 2020,
+
+
+	templateDir: '../shared/templates/'
 };
 
 module.exports = config;
