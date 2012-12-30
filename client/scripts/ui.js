111d9d9a3c83ab0e26376c0b2300fe4dcbcf9ac7
Autarc <Autarc@gmail.com>
2012-12-30 02:35:45 +0100

    create ui-script

---
+++ client/scripts/ui.js
@@ -0,0 +1,23 @@
+$(function(){
+
+
+
+	// navigation bar
+	// var linkContainer = $('#links'),
+
+	//	width = linkContainer.width(),
+
+	//	links = linkContainer.children().toArray(),
+
+	//	part = width/links.length;
+
+
+	// links.forEach( function ( link ) {
+
+	//	$(link).width( part );
+	// });
+
+
+
+
+});
